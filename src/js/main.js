7b9575c1a015dbbe5fe0b416997832f0e9f59924
sylvia_mavuba <smavuba@voyages-sncf.com>
2018-10-30 00:32:53 +0100

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ const state = {
     },
 
     board: {
-        cards: []
+        cards: [] //cartes totales de mon jeu
     },
 
     pioche: {
@@ -20,6 +20,12 @@ const state = {
     turn: 'player1',
 }
 
+
+
+/* -------- */
+/*
+Functions utils
+*/
 function shuffle(cards) {
     var arrCards = [];
     for (i = 0; i < cards.length; i++) {
@@ -31,10 +37,30 @@ function shuffle(cards) {
     return arrCards;
 }
 
-function removeCard(playerCard, card) {
-
+function updateStateCardBoard(cards, arrCardBoard) {
+    var index;
+    for (var i = 0; i < cards.length; i++) {
+        index = arrCardBoard.indexOf(arrCardBoard[i]);
+        if (index > -1) {
+            arrCardBoard.splice(index, 1);
+        }
+    }
 }
 
+function removeCard(monTasDeCartesAParser, laCarteASupprimer) {
+    //removeCard(state[player].cards, card)
+    for (var i = monTasDeCartesAParser.length - 1; i >= 0; i--) {
+        if (monTasDeCartesAParser[i] === laCarteASupprimer) {
+            monTasDeCartesAParser.splice(i, 1);
+        }
+    }
+}
+
+
+/* -------- */
+/*
+Init game functions
+*/
 function distributeCards(cardsNumbers, player) {
     //var cards = state.board.cards[0];
     var arrCards = [];
@@ -44,20 +70,71 @@ function distributeCards(cardsNumbers, player) {
     }
     console.log('le ', player, ' à ', arrCards.length, ' cartes. Voici la liste: ', arrCards);
 
-    // je met à jour la liste de mes cartes qui se trouvent sur la table
-    // en y supprimant les cartes distribuées au joueur 
-    boardCards.forEach(function(cardBoard) {
-        arrCards.forEach(function(card) {
-            if (cardBoard === card) {
-                boardCards.splice(card, 1)
-            }
-        })
-    });
+    // je met à jour la liste de mes cartes du jeu (board)
+    // en y supprimant les cartes qui viennent d'être distribuées au joueur 
+    updateStateCardBoard(arrCards, boardCards);
 
     //je met à jour le tableau des cartes du joueur 
     state[player].cards.push(arrCards);
     return arrCards;
+}
 
+function initCardOnBoard(numberCard) {
+
+    //je prend le tas de cartes du board et j'en affiche le nombre que je désire
+    var cardsBoard = state.board.cards[0];
+    var newArr = [];
+
+    for (var i = 0; i < numberCard; i++) {
+        newArr.push(cardsBoard[i]);
+    }
+
+    //je met à jour le nombre de cartes dans le jeu
+    updateStateCardBoard(newArr, cardsBoard) //=> PAS LA BONNE VALEUR !!
+
+    return newArr;
+}
+
+function initCardPioche(cards) {
+    state.pioche.cards = cards;
+    return cards;
+}
+
+
+/* -------- */
+/*
+Game Tour function
+*/
+function gameTour(player) {
+    //je met à jour le state du joueur à qui c'est le tour
+    state.turn = player;
+
+    //je place un event listener sur la div parent des cartes
+    $('.zone-player-bottom').addEventListener('click', cardClick, false);
+
+    function cardClick(e) {
+        var element = e.target;
+        //placer un data-attribut sur chaque carte générer
+
+        //je vérifie un tas de règle avant de décider si je peux jouer 
+        // if (...) => 
+        /*  
+            playCard(player, state[player].cards)
+            function playCard(player, card) {
+                removeCard(state[player].cards, card) //je supprime la carte cliquée du tas de cartes du joueur 
+                state.board.cards.push(card) // je la rajoute au tas de cartes du board
+                updatePioche()
+                updateCardBoard()
+            }
+        */
+        //je ne peux pas jouer  
+        // else 
+        /*
+            function cannotPlay(player, card) {
+                
+            }
+        */
+    }
 }
 
 if ('toutuntasdetests') {
@@ -65,10 +142,7 @@ if ('toutuntasdetests') {
 }
 
 
-/*function playCard(player, card) {
-    removeCard(state[player].cards, card)
-    state.board.cards.push(card)
-}*/
+
 
 window.onload = function() {
     //je mélange mes 76 cartes 
@@ -76,6 +150,7 @@ window.onload = function() {
 
     //je distribue 7 cartes au joueur 1
     distributeCards(7, "player1");
+    //renderCards(cards, locationHTML);
     //état de mon store 
     console.log('il me reste ', state.board.cards[0].length, ' après la distribution du player 1 sur la table à jouer');
 
@@ -86,4 +161,14 @@ window.onload = function() {
 
     //état de mon store 
     console.log('il me reste ', state.board.cards[0].length, 'sur la table à jouer');
+
+    //j'init une carte au hasard sur le board pour démarrer le jeu : il s'agit de la pile "cartes à jouer"
+    initCardOnBoard(1);
+    console.log('j\'ai ', initCardOnBoard(1).length, ' carte dans la pile "cartes à jouer" ');
+    //état de mon store 
+    console.log('il me reste ', state.board.cards[0].length, ' cartes sur la table à jouer');
+
+    //j'init la pile de cartes de la pioche = reste des cartes du board
+    initCardPioche(state.board.cards[0])
+    console.log('j\'ai ', initCardPioche(state.board.cards[0].length), ' de cartes dans la pioche')
 }
